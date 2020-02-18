408a187e60819de5957290980e46d1dbd519fdd7
Robert Smith <rsmithlal@gmail.com>
2020-02-18 00:27:15 -0500

    updated deploy script

--- deploy/build.sh
+++ deploy/build.sh
@@ -2,7 +2,5 @@
 
 yarn install --frozen-lockfile
 yarn build
-mv public public-vue # GitLab Pages hooks on the public folder
-mv dist public # rename the dist folder (result of npm run build)
 # optionally, you can activate gzip support wih the following line:
-find public -type f -regex '.*\.\(htm\|html\|txt\|text\|js\|css\)$' -exec gzip -f -k {} \;
+find dist -type f -regex '.*\.\(htm\|html\|txt\|text\|js\|css\)$' -exec gzip -f -k {} \;
