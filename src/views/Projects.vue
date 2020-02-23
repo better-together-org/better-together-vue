27ca0b26b85e2a489c6198c2c16790b4b0f5f8be
Robert Smith <rsmithlal@gmail.com>
2020-02-23 01:56:09 -0500

    Improved layout of the Projects section. Added an intro paragraph

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,57 +7,57 @@
     </div>
     <div class="row">
       <div class="col-md">
-        <ul>
-          <li>
-            <h5>
-              <a
-                href="https://github.com/better-together-org/community-engine"
-                target="bt-community-engine"
-                title="DRAW: Data Rescue Archives and Weather"
-              >
-                Better Together Community Engine
-              </a>
-            </h5>
-            <p>
-              The better together community engine is a Ruby on Rails plugin
-              that allows developers to build community features into their
-              apps with ease.
-            </p>
-          </li>
-          <li>
-            <h5>
-              <a
-                href="https://alpha.joatu.org/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
-                target="joatu"
-                title="Joatu"
-              >
-                Joatu Community Exchange
-              </a>
-            </h5>
-            <p>
-              Joatu is an application that allows people and organizations to
-              come together to exchange goods, services, and volunteering.
-              Joatu aims to work with community organizations to reduce waste
-              and excess consumption.
-            </p>
-          </li>
-          <li>
-            <h5>
-              <a
-                href="https://citsci.geog.mcgill.ca/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
-                target="draw"
-                title="DRAW: Data Rescue Archives and Weather"
-              >
-                DRAW: Data Rescue Archives and Weather
-              </a>
-            </h5>
-            <p>
-              DRAW is a citizen science project that aims to engage citizens
-              to recover historic weather observations from the archives and
-              make the data available for use in climate research.
-            </p>
-          </li>
-        </ul>
+        <p>
+          Better Together aims to bring people together to build great things!
+          These are some of the projects that Better Together and our partners have been working on.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md">
+        <h5>
+          <a
+            href="https://github.com/better-together-org/community-engine"
+            target="bt-community-engine"
+            title="DRAW: Data Rescue Archives and Weather"
+          >
+            Better Together Community Engine
+          </a>
+        </h5>
+        <p>
+          The better together community engine is a Ruby on Rails plugin
+          that allows developers to build community features into their
+          apps with ease.
+        </p>
+        <h5>
+          <a
+            href="https://alpha.joatu.org/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
+            target="joatu"
+            title="Joatu"
+          >
+            Joatu Community Exchange
+          </a>
+        </h5>
+        <p>
+          Joatu is an application that allows people and organizations to
+          come together to exchange goods, services, and volunteering.
+          Joatu aims to work with community organizations to reduce waste
+          and excess consumption.
+        </p>
+        <h5>
+          <a
+            href="https://citsci.geog.mcgill.ca/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
+            target="draw"
+            title="DRAW: Data Rescue Archives and Weather"
+          >
+            DRAW: Data Rescue Archives and Weather
+          </a>
+        </h5>
+        <p>
+          DRAW is a citizen science project that aims to engage citizens
+          to recover historic weather observations from the archives and
+          make the data available for use in climate research.
+        </p>
       </div>
     </div>
   </div>
