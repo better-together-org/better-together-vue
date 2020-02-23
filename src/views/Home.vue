acd434d3485c8becb741f82f3ad1207efaa9b134
Robert Smith <rsmithlal@gmail.com>
2020-02-23 01:56:40 -0500

    Added "an community powered by kindness in an engine for change" message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
     >
       <div class="row">
         <div class="col-md">
-          <h2>Kindness is better together!</h2>
+          <h2>Kindness is Better Together</h2>
         </div>
       </div>
       <div
@@ -14,6 +14,9 @@
         class="row"
       >
         <div class="col">
+          <p>
+            A community powered by kindness is an engine for change!
+          </p>
           <p>
             Better Together is an organization of progressive people and
             organizations working together to build a better world through
@@ -26,57 +29,49 @@
 
       <div id="projects">
         <h2>Our Projects</h2>
-        <ul>
-          <li>
-            <h5>
-              <a
-                href="https://github.com/better-together-org/community-engine"
-                target="bt-community-engine"
-                title="DRAW: Data Rescue Archives and Weather"
-              >
-                Better Together Community Engine
-              </a>
-            </h5>
-            <p>
-              The better together community engine is a Ruby on Rails plugin
-              that allows developers to build community features into their
-              apps with ease.
-            </p>
-          </li>
-          <li>
-            <h5>
-              <a
-                href="https://alpha.joatu.org/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
-                target="joatu"
-                title="Joatu"
-              >
-                Joatu Community Exchange
-              </a>
-            </h5>
-            <p>
-              Joatu is an application that allows people and organizations to
-              come together to exchange goods, services, and volunteering.
-              Joatu aims to work with community organizations to reduce waste
-              and excess consumption.
-            </p>
-          </li>
-          <li>
-            <h5>
-              <a
-                href="https://citsci.geog.mcgill.ca/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
-                target="draw"
-                title="DRAW: Data Rescue Archives and Weather"
-              >
-                DRAW: Data Rescue Archives and Weather
-              </a>
-            </h5>
-            <p>
-              DRAW is a citizen science project that aims to engage citizens
-              to recover historic weather observations from the archives and
-              make the data available for use in climate research.
-            </p>
-          </li>
-        </ul>
+        <h5>
+          <a
+            href="https://github.com/better-together-org/community-engine"
+            target="bt-community-engine"
+            title="DRAW: Data Rescue Archives and Weather"
+          >
+            Better Together Community Engine
+          </a>
+        </h5>
+        <p>
+          The better together community engine is a Ruby on Rails plugin
+          that allows developers to build community features into their
+          apps with ease.
+        </p>
+        <h5>
+          <a
+            href="https://alpha.joatu.org/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
+            target="joatu"
+            title="Joatu"
+          >
+            Joatu Community Exchange
+          </a>
+        </h5>
+        <p>
+          Joatu is an application that allows people and organizations to
+          come together to exchange goods, services, and volunteering.
+          Joatu aims to work with community organizations to reduce waste
+          and excess consumption.
+        </p>
+        <h5>
+          <a
+            href="https://citsci.geog.mcgill.ca/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
+            target="draw"
+            title="DRAW: Data Rescue Archives and Weather"
+          >
+            DRAW: Data Rescue Archives and Weather
+          </a>
+        </h5>
+        <p>
+          DRAW is a citizen science project that aims to engage citizens
+          to recover historic weather observations from the archives and
+          make the data available for use in climate research.
+        </p>
       </div>
 
       <div id="partners">
