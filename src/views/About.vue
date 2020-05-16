e957dc1dba5912e5312a7c0deb472d7987b69ae7
Robert Smith <rsmithlal@gmail.com>
2020-05-16 16:42:26 -0400

    update about page structure

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,29 +3,33 @@
     id="about"
     class="about container"
   >
-    <div class="row">
+    <div class="row heading">
       <div class="col-md">
         <h2>About</h2>
       </div>
     </div>
     <div class="row">
       <div class="col-md">
-        <p>
-          Better Together is a cooperative organization. It represents an
-          alliance of progressive people and teams who work together and
-          support one another in the pursuit of shared goals that help make
-          the world a better place for everyone.
-        </p>
-        <p>
-          In line with the values proposed in The Leap's Green New Deal,
-          Better Together works towards causes including climate justice and
-          reconciliation.
-        </p>
-        <p>
-          We promote local resilience by encouraging
-          people to be kind, communicate with and support one another, and
-          share their knowledge, skills, resources and time.
-        </p>
+        <div class="card">
+          <div class="card-body">
+            <p class="card-text">
+            Better Together is a cooperative organization. It represents an
+            alliance of progressive people and teams who work together and
+            support one another in the pursuit of shared goals that help make
+            the world a better place for everyone.
+            </p>
+            <p class="card-text">
+              In line with the values proposed in The Leap's Green New Deal,
+              Better Together works towards causes including climate justice and
+              reconciliation.
+            </p>
+            <p class="card-text">
+              We promote local resilience by encouraging
+              people to be kind, communicate with and support one another, and
+              share their knowledge, skills, resources and time.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
