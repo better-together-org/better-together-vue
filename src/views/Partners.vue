2860a9dfb736d9047f6090350d2d74c308c7b414
Robert Smith <rsmithlal@gmail.com>
2020-05-16 18:33:48 -0400

    Update partners appearance

--- src/views/Partners.vue
+++ src/views/Partners.vue
@@ -15,50 +15,78 @@
           we work alone! Every person and organization has their own
           strengths and weaknesses, but we're stronger together.
         </p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md">
         <p>
           Better Together is proud to partner with organizations that
           support our goals. If you would like to become a partner, please
           <a href="mailto: info@bettertogethersolutions.com">get in touch</a>.
         </p>
-        <p>
-          <a
-            href="http://alpha.joatu.org/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
-            target="joatu"
-            title="Joatu"
-          >
-            <img
-              src="assets/images/partners/joatu.png"
-              alt="Joatu"
-              width="300"
-            >
-          </a>
-          <a
-            href="https://citsci.geog.mcgill.ca/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
-            target="draw"
-            title="DRAW: Data Rescue Archives and Weather"
-          >
-            <img
-              src="assets/images/partners/DRAW.png"
-              alt="DRAW: Data Rescue Archives and Weather"
-              width="300"
-            >
-          </a>
-          <a
-            href="https://www.colibri-software.com/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
-            target="draw"
-            title="Colibri Software"
-          >
-            <img
-              src="assets/images/partners/colibri-software-logo-and-text-large.png"
-              alt="Colibri Software"
-              width="300"
-            >
-          </a>
-        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md">
+        <div class="card-deck">
+          <div class="card text-center">
+            <div class="card-header">
+              <h5>
+                Joatu
+              </h5>
+            </div>
+            <div class="card-footer h-100">
+              <a
+                href="http://alpha.joatu.org/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
+                target="joatu"
+                title="Joatu"
+              >
+                <img
+                  src="assets/images/partners/joatu.png"
+                  alt="Joatu"
+                  class='card-img-top'
+                >
+              </a>
+            </div>
+          </div>
+          <div class="card text-center">
+            <div class="card-header">
+              <h5>
+                DRAW
+              </h5>
+            </div>
+            <div class="card-footer h-100">
+              <a
+                href="https://citsci.geog.mcgill.ca/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
+                target="draw"
+                title="DRAW: Data Rescue Archives and Weather"
+              >
+                <img
+                  src="assets/images/partners/DRAW.png"
+                  alt="DRAW: Data Rescue Archives and Weather"
+                  class='card-img-top'
+                >
+              </a>
+            </div>
+          </div>
+          <div class="card text-center">
+            <div class="card-header">
+              <h5>
+                Colibri Software
+              </h5>
+            </div>
+            <div class="card-footer h-100">
+              <a
+                href="https://www.colibri-software.com/?utm_source=better-together&utm_medium=partner&utm_campaign=partnership"
+                target="draw"
+                title="Colibri Software"
+                class=""
+              >
+                <img
+                  src="assets/images/partners/colibri-software-logo-and-text-large.png"
+                  alt="Colibri Software"
+                  class='card-img-top'
+                >
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -71,3 +99,10 @@ export default {
   name: 'Partners',
 }
 </script>
+
+<style scoped lang="scss">
+  .card {
+    > a {
+    }
+  }
+</style>
