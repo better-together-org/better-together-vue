749bbd855fba2b8db9d86c9b4f2fa27ced471550
Robert Smith <rsmithlal@gmail.com>
2020-10-16 23:51:55 -0230

    Added contact form using netlify forms

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -16,8 +16,63 @@
             please send us an email at
             <a href="mailto: info@bettertogethersolutions.com">
               info@bettertogethersolutions.com
-            </a>
+            </a> or using the form below.
           </p>
+          <p>* means a field is required</p>
+
+          <form
+            class="form needs-validation"
+            data-netlify-recaptcha="true"
+            name="contact"
+            netlify
+          >
+            <div class="form-row mb-4">
+              <div class="col">
+                <label for="contact-name">Name</label>
+                <input
+                  class="form-control"
+                  id="contact-name"
+                  name="name"
+                  placeholder="Name"
+                  type="text"
+                >
+              </div>
+              <div class="col">
+                <label for="contact-email">Email *</label>
+                <input
+                  class="form-control"
+                  id="contact-email"
+                  name="email_address"
+                  placeholder="you@yoursite.com"
+                  type="email"
+                  required
+                >
+                <div class="invalid-feedback">
+                  Please use a valid email
+                </div>
+              </div>
+            </div>
+            <div class="form-row mb-4">
+              <div class="col">
+                <label for="contact-message">Message *</label>
+                <textarea
+                  class="form-control"
+                  cols="30"
+                  id="contact-message"
+                  name="message"
+                  rows="10"
+                  required
+                  ></textarea>
+              </div>
+            </div>
+            <div data-netlify-recaptcha="true"></div>
+            <button
+              class="btn btn-primary"
+              type="submit"
+            >
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
     </section>
