690248e86efb0476ff67f6fd759f08cee50c0329
Robert Smith <rsmithlal@gmail.com>
2021-01-23 12:00:10 -0330

    Moved some app styling out to a reusable theme scss file

--- src/App.vue
+++ src/App.vue
@@ -22,28 +22,29 @@ export default {
 @import 'bootstrap/scss/_functions.scss';
 @import 'bootstrap/scss/_variables.scss';
 @import 'bootstrap/scss/_mixins.scss';
+@import 'stylesheets/theme.scss';
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: $default-text-color;
 
   a {
-    color: #42b983;
+    color: $accent-color;
 
     &:hover {
-      color: #42b983;
+      color: $accent-color;
     }
   }
 
   .btn-primary {
-    color: #ffffff;
-    background-color: #42b983;
-    border-color: #42b983;
+    color: $default-text-color-bg-dark;
+    background-color: $accent-color;
+    border-color: $accent-color;
 
     &:hover, &:focus, &:active {
-      color: #ffffff;
+      color: $default-text-color-bg-dark;
       background-color: #399f71;
       border-color: #399f71;
     }
@@ -58,11 +59,11 @@ export default {
     .navbar-nav {
       a {
         font-weight: bold;
-        color: #ffffff;
+        color: $default-text-color-bg-dark;
 
         &.router-link-exact-active,
         &:hover {
-          color: #42b983;
+          color: $accent-color;
         }
       }
 
