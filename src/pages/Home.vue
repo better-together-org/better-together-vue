1032866d258bc6536179bdf8e69b6eceafda1f6f
Robert Smith <rsmithlal@gmail.com>
2021-02-08 20:02:06 -0330

    Reworked introduction section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,20 +9,15 @@
       <div class="col">
         <h2>We are Better Together</h2>
         <p>
-          Better Together is a cooperative organization. It represents an
-          alliance of progressive people and teams who work together and
-          support one another in the pursuit of shared goals that help make
-          the world a better place for everyone.
+          Better Together is a community innovation accelerator. We build
+          public community infrastructure to help make the world a
+          better place for everyone.
         </p>
         <p>
-          In line with the values proposed in The Leap's Green New Deal,
-          Better Together works towards causes including climate justice and
-          reconciliation.
-        </p>
-        <p>
-          We promote local resilience by encouraging
-          people to be kind, communicate with and support one another, and
-          share their knowledge, skills, resources and time.
+          We envision a
+          world where communities care for and support one another and share
+          their knowledge, skills, and resources to promote local resilience
+          and empower communities.
         </p>
       </div>
     </section>
