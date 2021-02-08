3853bc3264555a512df14af0d63342e84fde9c9f
Robert Smith <rsmithlal@gmail.com>
2021-02-08 19:55:04 -0330

    Removed projects from the home page
    Add a banner image

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -26,8 +26,11 @@
         </p>
       </div>
     </section>
-
-    <Projects />
+    <img
+      class="bt-banner img-fluid mx-auto d-block"
+      :src="BtBannerImg"
+      alt="Better Together"
+    >
     <Partners />
     <ContactNewsletter />
   </div>
@@ -35,16 +38,20 @@
 
 <script>
 // @ is an alias to /src
+import BtBannerImg from '../assets/bt-sunset-banner-1300x480px.jpg'
 import ContactNewsletter from '../components/ContactNewsletter.vue'
-import Projects from './Projects.vue'
 import Partners from './Partners.vue'
 
 export default {
   name: 'Home',
   components: {
     ContactNewsletter,
-    Projects,
     Partners,
+  },
+  data() {
+    return {
+      BtBannerImg,
+    }
   },
 }
 </script>
@@ -52,5 +59,10 @@ export default {
 <style scoped lang="scss">
   h2 {
     margin-bottom: 1.5rem;
+  }
+
+  .bt-banner {
+    margin-bottom: 15px;
+    border-radius: 10px;
   }
 </style>
