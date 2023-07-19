8df62c571e07d77e61a22e30277227b8a441d774
Robert Smith <rsmithlal@gmail.com>
2023-07-19 21:28:38 -0230

    check for and create dist folder when building

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,6 +3,11 @@
 echo "VUE_APP_BETTER_TOGETHER_API_URI=${VUE_APP_BETTER_TOGETHER_API_URI}" >> ./.env
 
 yarn install --frozen-lockfile
+
+if [ ! -d "dist" ]; then
+    mkdir dist
+fi
+
 yarn build
 # optionally, you can activate gzip support wih the following line:
 find dist -type f -regex '.*\.\(htm\|html\|txt\|text\|js\|css\)$' -exec gzip -f -k {} \;
